17a80c8a1e98fab3b746c2f9e3cbb7e320010837
Sanju afre <148860124+sanjuafre123@users.noreply.github.com>
2023-11-07 22:01:53 +0530

    Add files via upload

---
+++ Switch-Case/Month.c
@@ -0,0 +1,39 @@
+#include<stdio.h>
+
+main()
+{
+	
+	int Months;
+	printf("Enter The Values : ");
+	scanf("%d",&Months);
+	
+	switch(Months)
+	{
+		case 1 : printf("January - 31 ");
+		break;
+		case 2 : printf("February - 28 ");
+		break;
+		case 3 : printf("March - 31 ");
+		break;
+		case 4 : printf("April - 30 ");
+		break;
+		case 5 : printf("May - 31 ");
+		break;
+		case 6 : printf("June - 30 ");
+		break;
+		case 7 : printf("July - 31 ");
+		break;
+		case 8 : printf("August - 31 ");
+		break;
+		case 9 : printf("September - 30 ");
+		break;
+		case 10 : printf("October - 31 ");
+		break;
+		case 11: printf("November - 30 ");
+		break;
+		case 12 : printf("December - 31 ");
+		break;
+		default: printf("Enter The Valid input Value ");
+	}
+	
+}
---
+++ Switch-Case/call ser.c
@@ -0,0 +1,81 @@
+#include <stdio.h>
+
+main() 
+{
+	
+   int a;
+   printf("Welcome Customer Care\n");
+   printf("Enter The number : ");
+   scanf("%d",&a);
+   
+   switch(a)
+   {
+   	 case 1 : printf("Choose The Language ? \n");
+   	 int lan;
+   	 printf("you Enter 1 For English And 2 enter For Hindi : \n");
+     scanf("%d",&lan);
+     
+     switch(lan)
+     {
+     	case 1 : printf("English");
+     	break;
+     	case 2 : printf("Hindi");
+		break;
+		default : printf("Enter The Value 1 to 2"); 
+	 }
+   	 break;
+   	 
+   	 case 2 : printf("Sim Card Problem\n");
+   	 int sim;
+   	 printf("Enter 1 For New Sim Card Or 2 For Port Sim Card\n");
+   	 scanf("%d",&sim);
+   	 
+   	 switch(sim)
+   	 {
+   	 	case 1 : printf("We Have Accepted Your Request Your New Sim Card Will Delevired Soon...");
+   	 	break;
+   	 	case 2 : printf("We Have Accepted Your Request Your Sim Will Be Port Soon...");
+		break;
+		deafult : printf("Enter The Value Between 1 to 2");
+	 }
+	 break;
+	 
+   	 case 3 : printf("internet connection\n");
+   	 int net;
+   	 printf("Enter  enter interner problems \n");
+   	 printf("\n1. slow speed");
+	 printf("\n2. internet not working ");
+
+   	 scanf("%d",&net);
+   	 
+   	 switch(net)
+   	 {
+   	 	case 1 : printf("slow speed");
+   	 	break;
+   	 	case 2 : printf("internet not working");
+		break;
+		default : printf("enter the valid value");
+		break;
+	 }
+	 break;
+   	 
+   	 case 4 : printf("For Recharge\n");
+   	 int rec;
+   	 printf("Recharge Plan\n");
+   	 scanf("%d",&rec);
+   	 
+   	 switch(rec)
+   	 {
+   	 	case 1 : printf("Rs. 239 - For 28 Days");
+   	 	break;
+   	 	case 2 : printf("Rs. 666 - For 84 Days");
+		break;
+		case 3 : printf("Rs. 2199 - For 1 Year");
+		break;
+		default : printf("Enter The Value 1 to 3");
+	 }
+	 break;
+	 default : printf("Enter The Value 1 to 4");
+	 
+}
+}
