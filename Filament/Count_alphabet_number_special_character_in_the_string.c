64881ad0e3f85ac829e4cd2c31222d84c8d6ba5a
Sanju afre <148860124+sanjuafre123@users.noreply.github.com>
2023-12-15 14:41:01 +0500

    Update Count_alphabet_number_special_character_in_the_string.c

--- Filament/Count_alphabet_number_special_character_in_the_string.c
+++ Filament/Count_alphabet_number_special_character_in_the_string.c
@@ -4,30 +4,27 @@
 
 void main()
 {
-    char name[50];//string declare
+    char name[50];
     printf("Enter the string : ");
-    gets(name);//user prompt
-    int length=strlen(name);//length of string
-    int i;//loop counter
-    int alpha,num,sp;//alphabet,number and special character count
+    gets(name);
+    int length=strlen(name);
+    int i;
+    int alpha,num,sp;
     
     for(i=0; i<length; i++)
     {
-        //alphabet count
         if(name[i]>=65 && name[i]<=90 || name[i]>=97 && name[i]<=122)
         {
             alpha++;
         }
-        //number count
         else if(name[i]>=48 && name[i]<=57)
         {
             num++;
         }
-        //special character count
         else if(name[i]>=33 && name[i]<=47 || name[i]>=58 && name[i]<=64)
         {
             sp++;
         }
     }
-    printf("\nAlphabet in the string : %d \n\nNumber in the string : %d \n\nSpecial character in the string : %d",alpha,num,sp);
+    printf("\nAlphabet is: %d \n\nNumber is : %d \n\nSpecial character is : %d",alpha,num,sp);
 }
